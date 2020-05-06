45f1223eb06f7b6c20f4fbe4f24754ff6e24e248
Dan Fabulich <dan.fabulich@redfin.com>
2020-05-05 17:10:31 -0700

    trying to configure JSX, failing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+/** @jsx h */
+
 /**
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
 import confetti from "canvas-confetti";
+
+function h(name, props, children) {
+	return {name, props, children};
+}
+
+console.log(<h1 id="foo">Hello world</h1>);
 
 const canvas = document.createElement("canvas");
 canvas.width = "960";
